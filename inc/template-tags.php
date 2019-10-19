69e1da06850125593b453a19e5b4e19ea2f34020
Marcus Kazmierczak <marcus@mkaz.com>
2019-10-19 07:47:19 -0700

    Remove edit links from lists

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -70,24 +70,6 @@ if ( ! function_exists( 'miningtown_entry_meta' ) ) :
 				); // WPCS: XSS OK.
 			}
 		}
-
-		// Edit post link.
-		edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers. */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'miningtown' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			),
-			'<span class="edit-link">' . TwentyNineteen_SVG_Icons::get_svg( 'ui', 'edit', 16 ),
-			'</span>'
-		);
 	}
 endif;
 
@@ -127,24 +109,6 @@ if ( ! function_exists( 'miningtown_entry_footer' ) ) :
 				); // WPCS: XSS OK.
 			}
 		}
-
-		// Edit post link.
-		edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers. */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'miningtown' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			),
-			'<span class="edit-link">' . TwentyNineteen_SVG_Icons::get_svg( 'ui', 'edit', 16 ),
-			'</span>'
-		);
 	}
 endif;
 
