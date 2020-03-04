5aa14a4ca75573adca4e1ff219a413d8eaac45da
Marcus Kazmierczak <marcus@mkaz.com>
2020-03-04 09:40:35 -0800

    Add RSS feed theme support

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@
 
 add_action( 'after_setup_theme', function() {
 
+	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'editor-styles' );
 	add_theme_support( 'title-tag' );
 	add_editor_style( 'style-editor.css' );
