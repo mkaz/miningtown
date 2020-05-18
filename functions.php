f6a2e5ef68c0a257307d311111d7d088ae76bc42
Marcus Kazmierczak <marcus@mkaz.com>
2020-05-18 12:03:39 -0700

    Rework widgets and footer

--- functions.php
+++ functions.php
@@ -169,24 +169,6 @@ add_action( 'widgets_init', function() {
 		'after_title'   => '</h5>',
 	) );
 
-	register_sidebar( array(
-		'name'          => 'Author Bio',
-		'id'            => 'author-bio',
-		'before_widget' => '<div>',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h2>',
-		'after_title'   => '</h2>',
-	) );
-
-	register_sidebar( array(
-		'name'          => 'Follow Block',
-		'id'            => 'follow-block',
-		'before_widget' => '<div>',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h2>',
-		'after_title'   => '</h2>',
-	) );
-
 } );
 
 // No says text
